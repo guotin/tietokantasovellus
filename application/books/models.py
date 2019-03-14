06e2063c0ece1012d0e44a00b8448f974e690acc
guotin <eerokauhanen@outlook.com>
2019-03-14 10:50:51 +0200

    kirjan attribuutin tyypin vaihto

--- application/books/models.py
+++ application/books/models.py
@@ -6,7 +6,7 @@ class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(144), nullable=False)
     author = db.Column(db.String(144), nullable=False)
-    publication_year = db.Column(db.String(10), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
 
     def __init__(self, name, author, publication_year):
         self.name = name
