e310b99df51980d06c44eea269b01a2b132ed68b
guotin <eerokauhanen@outlook.com>
2019-03-27 08:54:12 +0200

    Added validators for textfields to not allow empty input

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -9,8 +9,8 @@ class LoginForm(FlaskForm):
         csrf = False
         
 class RegisterForm(FlaskForm):
-    username = StringField("Username", [validators.Length(min=2, max=10)])
-    password = PasswordField("Password", [validators.Length(min=4, max=20)])
+    username = StringField("Username", [validators.Length(min=2, max=10), validators.DataRequired()])
+    password = PasswordField("Password", [validators.Length(min=4, max=20), validators.DataRequired()])
 
     class Meta:
         csrf = False
--- application/books/forms.py
+++ application/books/forms.py
@@ -2,16 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, validators
 
 class BookForm(FlaskForm):
-    name = StringField("Book title", [validators.Length(min=2)])
-    author = StringField("Book author", [validators.Length(min=2)])
+    name = StringField("Book title", [validators.Length(min=2), validators.DataRequired()])
+    author = StringField("Book author", [validators.Length(min=2), validators.DataRequired()])
     publication_year = IntegerField("Publication year")
 
     class Meta:
         csrf = False
         
 class UpdateForm(FlaskForm):
-    name = StringField("Book title", [validators.Length(min=2)])
-    author = StringField("Book author", [validators.Length(min=2)])
+    name = StringField("Book title", [validators.Length(min=2), validators.DataRequired()])
+    author = StringField("Book author", [validators.Length(min=2), validators.DataRequired()])
     publication_year = IntegerField("Publication year")
 
     class Meta:
